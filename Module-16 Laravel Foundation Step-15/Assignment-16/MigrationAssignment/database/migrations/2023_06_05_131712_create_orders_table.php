1228ba46d0e3a7693bb043c287d76eeda9de3b30
junaid-mij <junath57@gmail.com>
2023-06-05 19:42:42 +0600

    update

--- Module-16 Laravel Foundation Step-15/Assignment-16/MigrationAssignment/database/migrations/2023_06_05_131712_create_orders_table.php
+++ Module-16 Laravel Foundation Step-15/Assignment-16/MigrationAssignment/database/migrations/2023_06_05_131712_create_orders_table.php
@@ -4,20 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name',200);
-            $table->decimal('price');
-            $table->text('description');
-            //Modify
-            $table->integer('quantity')->nullable();
+            $table->unsignedInteger('product_id');
+            $table->integer('quantity');
+            //Realtion
+            $table->foreign('product_id')->references('id')->on('products');
 
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
@@ -29,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('orders');
     }
 };
