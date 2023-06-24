a5dfb939dbf4bcf2c512aa889b4895c5ecfb9d5c
junaid-mij <junath57@gmail.com>
2023-06-24 16:02:53 +0600

    new

---
+++ Module-15 Laravel Foundation Step-14/Practice-BladeTemplate/resources/views/Home.blade.php
@@ -0,0 +1,9 @@
+@extends('Layout.app')
+
+@section('content')
+    @include('Content.hero')
+    @include('Content.howWeDo')
+    @include('Content.pricing')
+    @include('Content.team')
+    @include('Content.newsLatter')
+@endsection
--- Module-15 Laravel Foundation Step-14/Practice-BladeTemplate/routes/web.php
+++ Module-15 Laravel Foundation Step-14/Practice-BladeTemplate/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\DemoController;
-use App\Http\Controllers\GroupController;
-use App\Http\Middleware\DemoMiddleware;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,14 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/demo/{key}',[DemoController::class,'DemoAction'])->middleware(DemoMiddleware::class);
-Route::get('/fail',[DemoController::class,'DemoAction2']);
-
-
-//MiddleWare Grouping:
-Route::middleware('CheckRequestDetails')->group(function(){
-    Route::get('/demo1/{key}',[GroupController::class,'DemoAction1']);
-    Route::get('/demo2/{key}',[GroupController::class,'DemoAction2']);
-    Route::get('/demo3/{key}',[GroupController::class,'DemoAction3']);
-    Route::get('/demo4/{key}',[GroupController::class,'DemoAction4']);
-});
+//Blade-template-test:
+Route::get('/home',[HomeController::class,'page']);
