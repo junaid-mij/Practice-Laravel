801ce89f653ea4811485c1219f34de2e42fd6428
junaid-mij <junath57@gmail.com>
2025-10-12 23:51:33 +0600

    ohhh

--- Module-23 Laravel Ecommerce Project Step-22/Practice/E-Commerce/routes/web.php
+++ Module-23 Laravel Ecommerce Project Step-22/Practice/E-Commerce/routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PolicyController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenAuthenticate;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -48,3 +50,8 @@ Route::get('/PolicyByType/{type}', [PolicyController::class, 'PolicyByType']);
 Route::get('/UserLogin/{UserEmail}', [UserController::class, 'UserLogin']);
 Route::get('/VerifyLogin/{UserEmail}/{OTP}', [UserController::class, 'VerifyLogin']);
 Route::get('/logout', [UserController::class, 'UserLogout']);
+
+
+// User Profile:
+Route::post('/CreateProfile', [ProfileController::class, 'CreateProfile'])->middleware([TokenAuthenticate::class]);
+Route::get('/ReadProfile', [ProfileController::class, 'ReadProfile'])->middleware([TokenAuthenticate::class]);
